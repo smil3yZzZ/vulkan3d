dd3d41cb66701879a7fa1387939e9338477752f2
Héctor Olivera Pazó <holivera@optaresolutions.com>
2021-11-25 18:04:28 +0100

    First commit

---
+++ VulkanTest/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.2.189.2\Bin32\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+C:\VulkanSDK\1.2.189.2\Bin32\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+pause
