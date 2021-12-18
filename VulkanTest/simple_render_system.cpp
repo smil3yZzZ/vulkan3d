fbc8504424122150c8345963480c0d22b59ed951
Héctor Olivera Pazó <h.oliverap94@gmail.com>
2021-12-18 12:44:26 +0100

    Custom camera added

--- VulkanTest/simple_render_system.cpp
+++ VulkanTest/simple_render_system.cpp
@@ -64,13 +64,15 @@ namespace lve {
 	void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject> &gameObjects, const LveCamera& camera) {
 		lvePipeline->bind(commandBuffer);
 
+		auto projectionView = camera.getProjection() * camera.getView();
+
 		for (auto& obj : gameObjects) {
 			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
 			obj.transform.rotation.x = glm::mod(obj.transform.rotation.y + 0.001f, glm::two_pi<float>());
 
 			SimplePushConstantData push{};
 			push.color = obj.color;
-			push.transform = camera.getProjection() * obj.transform.mat4();
+			push.transform = projectionView * obj.transform.mat4();
 
 			vkCmdPushConstants(
 				commandBuffer,
